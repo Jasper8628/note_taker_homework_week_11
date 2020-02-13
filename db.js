2b10181b0de25b6a9ba4a0701efaec1254612121
jasper <msavantfou@gmail.com>
2020-02-13 22:04:50 +1100

    commit

---
+++ db.js
@@ -0,0 +1,16 @@
+let data=[{
+    date:"some date",
+    time:"some time",
+    content:"some content",
+    id:1
+    
+},
+{
+    date:"some date",
+    time:"some time",
+    content:"some content",
+    id:2
+
+}
+];
+module.exports=data;
--- server.js
+++ server.js
@@ -3,23 +3,44 @@ var path = require("path");
 var fs = require("fs");
 var app = express();
 var PORT = 3000;
-
+let data=require("./db.js");
 
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 
 app.get("/",function(req,res){
+    res.sendFile(path.join(__dirname, "index.html"));
 
 });
-
-app.get("/notes",function(req,res){
-
+app.get("/api/notes",function(req,res){
+    console.log(data);
+    res.json(data);
+});
+app.post("/api/notes",function(req,res){
+    let newNote=req.body;
+    data.push(newNote);
+    res.json(data);
 });
 
-app.get("api/notes",function(req,res){
+
+
+app.post("/notes/delete",function(req,res){
+    let noteId=req.body;
+    for(entry of data){
+        if(entry.id=noteId){
+            let index=data.indexOf(entry);
+            data.splice(index,1);
+        }
+    }
+    res.json(data);
 
 });
+app.post("/api/notes/delete-all",function(req,res){
+    data=[];
+    res.json(data);
+});
 
-app.post("api/notes",function(req,res){
 
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
 });

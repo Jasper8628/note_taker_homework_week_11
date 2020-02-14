f5cf7a8450cf47c53a6bf10e34d44523b6749a1e
jasper <msavantfou@gmail.com>
2020-02-14 17:23:51 +1100

    mm

--- server.js
+++ server.js
@@ -2,45 +2,50 @@ var express = require("express");
 var path = require("path");
 var fs = require("fs");
 var app = express();
-var PORT = process.env.PORT||3000;
-let data=require("./db.js");
+var PORT = process.env.PORT || 3000;
+let data = require("./db.js");
 
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.get("/",function(req,res){
+app.get("/", function (req, res) {
     res.sendFile(path.join(__dirname, "index.html"));
 
 });
-app.get("/api/notes",function(req,res){
-    console.log(data);
+app.get("/api/notes", function (req, res) {
     res.json(data);
 });
-app.post("/api/notes",function(req,res){
-    let newNote=req.body;
-    data.push(newNote);
+app.post("/api/notes", function (req, res) {
+    let newNote = req.body;
+    let index=data.findIndex(entry=>entry.id==newNote.id);
+    if (index==-1) {
+        data.push(newNote);
+    }
+    else{
+        data[index].content=newNote.content;
+    }
     res.json(data);
 });
 
 
 
-app.post("/notes/delete",function(req,res){
-    let noteId=req.body;
-    for(entry of data){
-        if(entry.id=noteId){
-            let index=data.indexOf(entry);
-            data.splice(index,1);
+app.post("/notes/delete", function (req, res) {
+    let noteId = req.body;
+    for (entry of data) {
+        if (entry.id = noteId) {
+            let index = data.indexOf(entry);
+            data.splice(index, 1);
         }
     }
     res.json(data);
 
 });
-app.post("/api/notes/delete-all",function(req,res){
-    data=[];
+app.post("/api/notes/delete-all", function (req, res) {
+    data = [];
     res.json(data);
 });
 
 
-app.listen(PORT, function() {
-  console.log("App listening on PORT: " + PORT);
+app.listen(PORT, function () {
+    console.log("App listening on PORT: " + PORT);
 });

bef096c4a0a078ca51c7a63bacbf6068bec7a9ee
jasper <msavantfou@gmail.com>
2020-03-18 22:04:06 +1100

    pwa

--- server.js
+++ server.js
@@ -2,9 +2,10 @@ var express = require("express");
 var path = require("path");
 var fs = require("fs");
 var app = express();
-var PORT = process.env.PORT || 3000;
+var PORT = process.env.PORT || 8080;
 let data = require("./db.js");
 
+app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
